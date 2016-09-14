4e12ef220e311a790d6e0b50dd6543c3dd7a1797
Bharani <bharani91@gmail.com>
2016-09-14 13:37:18 +0530

    Minor typo fix

--- config/prod.exs
+++ config/prod.exs
@@ -59,7 +59,7 @@ config :logger, level: :info
 #
 
 
-config :hello_phoenix, HelloPhoenix.Repo,
+config :ap, Ap.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
