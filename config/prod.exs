851567d95c23b8613997690dd3b22fa3419e0c22
Bharani <bharani91@gmail.com>
2016-09-18 00:34:15 +0530

    Changing config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :ap, Ap.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "www.authenticpixels.com", port: 443],
+  url: [host: System.get_env("HOSTNAME"), port: 80]
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
